39aad6dc204ddce20a9a2f15e3cf60ab43a615ea
MartinStevo <50314138+MartinStevo@users.noreply.github.com>
2021-02-27 18:50:55 +0100

    Create barrier_event.py

---
+++ barrier_event.py
@@ -0,0 +1,39 @@
+from random import randint
+from time import sleep
+from fei.ppds import Thread, Mutex, Event
+from fei.ppds import print
+
+
+class SimpleBarrier:
+    def __init__(self, N):
+        self.N = N
+        self.c = 0
+        self.m = Mutex()
+        self.b = Event()
+
+    def wait(self):
+        self.m.lock()
+        self.c += 1
+        if (self.c == self.N):
+            self.b.signal()
+        self.m.unlock()
+        self.b.wait()
+
+
+def barrier_example(barrier, thread_id):
+    sleep(randint(1, 10) / 10)
+    print("vlakno %d pred barierou" % thread_id)
+    barrier.wait()
+    print("vlakno %d po bariere" % thread_id)
+
+
+n = 5
+sb = SimpleBarrier(n)
+
+threads = list()
+for i in range(n):
+    t = Thread(barrier_example, sb, i)
+    threads.append(t)
+
+for i in threads:
+    t.join()
